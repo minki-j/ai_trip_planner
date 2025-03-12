71a3d2226f95829797ce8785f935cf90646083cb
Minki Jung <qmsoqm2@gmail.com>
2025-03-12 10:10:28 -0400

    wip: mcp server

---
+++ backend/app/utils/mcp.py
@@ -0,0 +1,54 @@
+from contextlib import AsyncExitStack
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+class MCPClient:
+    def __init__(self):
+        # Initialize session and client objects
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.anthropic = Anthropic()
+        
+    async def connect_to_server(self, python_server_script_path: str):		
+        server_params = StdioServerParameters(
+            command="python",
+            args=[python_server_script_path],
+            env=None
+        )
+    
+        stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
+        self.stdio, self.write = stdio_transport
+        self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
+    
+        await self.session.initialize()
+    
+        # List available tools
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("\nConnected to server with tools:", [tool.name for tool in tools])
+        
+    async def process_query(self, query: str) -> str:
+        """Process a query using Claude and available tools"""
+        messages = [
+            {
+                "role": "user",
+                "content": query
+            }
+        ]
+    
+            # Accessing the tool information from the MCP server.
+        response = await self.session.list_tools()
+        available_tools = [{
+            "name": tool.name,
+            "description": tool.description,
+            "input_schema": tool.inputSchema
+        } for tool in response.tools]
+    
+        # Calling Anthropic Claude Sonnet 
+        response = self.anthropic.messages.create(
+            model="claude-3-5-sonnet-20241022",
+            max_tokens=1000,
+            messages=messages,
+            tools=available_tools
+        )
